6e4663d8ce150dfed84bd4b6b682f5361bfc06cf
Filip <fhagno@yahoo.com>
2025-02-28 14:30:39 +0100

    Add oauth security

--- src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -5,24 +5,36 @@ import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConf
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+
 
 @Configuration
 public class RestTemplateBuilderConfig {
-
-    @Value("${rest.template.user}")
-    String username;
-
-    @Value("${rest.template.password}")
-    String password;
 
     @Value("${rest.template.rootUrl}")
     String rootUrl;
 
     @Bean
-    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
-        return configurer.configure(new RestTemplateBuilder()
-                .basicAuthentication(username, password))
+    OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
+                                                          OAuth2AuthorizedClientService authorizedClientService) {
+        OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+
+        AuthorizedClientServiceOAuth2AuthorizedClientManager manager
+                = new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientService);
+        manager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return manager;
+    }
+
+    @Bean
+    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer,
+                                            OAuthClientInterceptor interceptor) {
+        return configurer.configure(new RestTemplateBuilder())
+                .additionalInterceptors(interceptor)
                 .uriTemplateHandler(new DefaultUriBuilderFactory(rootUrl));
     }
 }
